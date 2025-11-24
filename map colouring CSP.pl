d9727c9a7a466571159e3cb18142c254b23b61f6
Shree278 <mangeshnimbalkar1972@gmail.com>
2025-11-24 09:14:09 +0530

    Update map colouring CSP.pl

--- map colouring CSP.pl
+++ map colouring CSP.pl
@@ -1,17 +1,10 @@
 :- use_module(library(clpfd)).
 
 solve_australia_coloring(Coloring) :-
-    %1. Define the variables: one for each state/territory.
-    % The variables names are uppercase , which is standard for Prolog.
     Coloring = [WA, NT, SA, QLD, NSW, VIC],
-
-    %2. Define the domain for each variables (the available colors).
-    % we use integer values to represent the collors: 1=red, 2=green, 3=blue.
     Coloring ins 1..3,
-
-    %3. Define the constraints: adjacent states must have different colors.
-    WA #\= NT,  %Western Australia must have a differet color than Nortern Territory.
-    WA #\= SA,  %Western Australia must have a differet color than South Territory.
+    WA #\= NT,
+    WA #\= SA,
     NT #\= SA,
     NT #\= QLD,
     SA #\= QLD,
@@ -19,15 +12,12 @@ solve_australia_coloring(Coloring) :-
     SA #\= VIC,
     QLD #\= NSW,
     NSW #\= VIC,
-
-    %4. Find the first solutin by labeling the variables.
-    % The 'labeling' predicates trigers the search for a solution.
     labeling([], Coloring).
 
-%A helper predicates to display the solution in a more readable format.
 display_coloring([WA, NT, SA, QLD, NSW, VIC]) :-
-
     format('Western Australia: ~w~n', [WA]),
     format('North Territory: ~w~n', [NT]),
     format('South Australia: ~w~n', [SA]),
     format('Queensland: ~w~n', [QLD]),
+    format('New South Wales: ~w~n', [NSW]),
+    format('Victoria: ~w~n', [VIC]).
