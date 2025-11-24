b66e98d9eb965ed9d242d2fc44a4434b45147a87
Shree278 <mangeshnimbalkar1972@gmail.com>
2025-11-24 09:15:46 +0530

    Refactor 8 queens problem implementation

--- 8 queens problem.pl
+++ 8 queens problem.pl
@@ -1,28 +1,20 @@
- :- use_module(library(lists)).   % permutations/2
+:- use_module(library(lists)).
 
-% queens(Qs) - Qs is a list of 8 integers; element
 queens(Qs) :-
     permutation([1,2,3,4,5,6,7,8], Qs),
     safe(Qs).
 
-% safe(List) - no two queens attack each other diagonally
 safe([]).
 safe([Q|Qs]) :-
     no_attack(Q, Qs, 1),
     safe(Qs).
 
-% no_attack(Q, Rest, D) - Q doesn't attack any element of rest;
-% D is the column distance between Q and the head of Rest (1 for
-% adjacent column).
 no_attack(_, [], _).
 no_attack(Q, [Q1|Qs], D) :-
     Q =\= Q1,
     abs(Q - Q1) =\= D,
-
     D1 is D + 1,
     no_attack(Q, Qs, D1).
-
-% optional: print a human-readable board for solution Qs
 
 print_Board(Qs) :-
     length(Qs, N),
